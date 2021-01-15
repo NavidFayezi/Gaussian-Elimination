a7c9c44f6d33d738c7c4b0037426c043344a8306
NavidFayezi <navidfayezi.98@gmail.com>
2021-01-15 16:28:39 +0330

    add gaussian elimination with partial pivoting

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,15 +1,19 @@
-files = []
-files.append(open("A_1.txt", "r").read().split('\n'))
-files.append(open("A_2.txt", "r").read().split('\n'))
-files.append(open("A_3.txt", "r").read().split('\n'))
+files = [open("A_1.txt", "r").read().split('\n'),
+         open("A_2.txt", "r").read().split('\n'),
+         open("A_3.txt", "r").read().split('\n')]
 
-matrices = []   # all three matrices will be stored in this 3D array
+load_vector = [list(map(float, open("b_1.txt", "r").read().split('\n'))),
+               list(map(float, open("b_2.txt", "r").read().split('\n'))),
+               list(map(float, open("b_3.txt", "r").read().split('\n')))]
+
+matrices = []                   # coefficient matrices will be stored in this 3D array.
 for i in range(len(files)):
     matrices.append([])
     temp = []
     for j in range(len(files[i])):
         files[i][j] = [x for x in files[i][j].split(' ') if x != '']
         temp.append(list(map(float, files[i][j])))
+        temp[j].append(load_vector[i][j])                   # augment the matrix with load.
     matrices[i] = temp[:]
 
 
@@ -25,3 +29,25 @@ def partial_pivoting(matrix, col):
     matrix[index] = temp
 
 
+def gaussian_elimination_partial_pivoting(matrix):
+    matrix_size = len(matrix)                   # matrix_size == number of rows.
+    # forward elimination
+    for col in range(matrix_size-1):
+        partial_pivoting(matrix, col)
+        for row in range(col+1, matrix_size):
+            multiple = -1 * (matrix[row][col] / matrix[col][col])
+            matrix[row][col] = 0                    # explicitly assign zero to this element to avoid numerical errors.
+            for k in range(col+1, matrix_size+1):                   # increase the range by 1 to include the load.
+                matrix[row][k] += multiple * matrix[col][k]
+    # backward substitution
+    answer = [0.0 for x in range(matrix_size)]
+    answer[matrix_size-1] = matrix[matrix_size-1][matrix_size] / matrix[matrix_size-1][matrix_size-1]
+    for i in range(matrix_size-2, -1, -1):
+        sigma = 0.0
+        for j in range(i+1, matrix_size):
+            sigma += matrix[i][j] * answer[j]
+        answer[i] = (matrix[i][-1] - sigma) / matrix[i][i]
+    return answer
+
+
+print(gaussian_elimination_partial_pivoting(matrices[0]))
