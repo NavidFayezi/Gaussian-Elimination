8d6d15b1a5728aeed22628167e29653fe25e2d3e
NavidFayezi <navidfayezi.98@gmail.com>
2021-01-15 23:15:59 +0330

    add gauss-jordan method

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -23,6 +23,7 @@ def initialize():
 
 
 def read_augment_inputs():
+    global answers, b_new, files, load_vector, matrices, a_coef
     for i in range(len(files)):
         matrices.append([])
         a_coef.append([])
@@ -62,7 +63,7 @@ def scaled_partial_pivoting(matrix, col):
     matrix[index] = temp
 
 
-def gaussian_elimination_partial_pivoting(matrix, pivoting_function):
+def gaussian_elimination(matrix, pivoting_function):
     assert len(matrix) == (len(matrix[0]) - 1)                  # check the size of augmented matrix.
     try:
         matrix_size = len(matrix)                   # matrix_size == number of rows.
@@ -106,16 +107,68 @@ def matrix_multiplication(a, b):                    # a * b
     return result
 
 
+def gauss_jordan(matrix, pivoting_function):
+    assert len(matrix) == (len(matrix[0]) - 1)                  # check the size of augmented matrix.
+    try:
+        matrix_size = len(matrix)                   # matrix_size == number of rows.
+        # forward elimination
+        for col in range(matrix_size-1):
+            pivoting_function(matrix, col)
+            for row in range(col+1, matrix_size):
+                multiple = -1 * (matrix[row][col] / matrix[col][col])
+                matrix[row][col] = 0                    # explicitly assign zero to this element to avoid numerical errors.
+                for k in range(col+1, matrix_size+1):                   # increase the range by 1 to include the load.
+                    matrix[row][k] += multiple * matrix[col][k]
+        # backward elimination
+        for col in range(matrix_size-1, 0, -1):
+            for row in range(col-1, -1, -1):
+                multiple = -1 * (matrix[row][col] / matrix[col][col])
+                for k in range(matrix_size, col-1, -1):
+                    matrix[row][k] += multiple * matrix[col][k]
+                matrix[row][col] = 0                    # explicitly assign zero to this element to avoid numerical errors.
+        answer = [[0] for x in range(matrix_size)]
+        for i in range(matrix_size):
+            answer[i] = [matrix[i][matrix_size] / matrix[i][i]]
+        return answer
+    except Exception as e:
+        if e.__class__ == ZeroDivisionError:
+            print("Inconsistent equation")
+        else:
+            print("something else went wrong. ", e.__str__())
+
+
 if __name__ == "__main__":
-    initialize()
-    read_augment_inputs()
-    for i in range(3):
-        answers.append(gaussian_elimination_partial_pivoting(matrices[i], partial_pivoting))
-        b_new.append(matrix_multiplication(a_coef[i], answers[i]))
-        print(b_new[i])
-    initialize()
-    read_augment_inputs()
-    for i in range(3):
-        answers.append(gaussian_elimination_partial_pivoting(matrices[i], scaled_partial_pivoting))
-        b_new.append(matrix_multiplication(a_coef[i], answers[i]))
-        print(b_new[i])
+    global answers, b_new, files, load_vector, matrices, a_coef
+    pivot_functions = [partial_pivoting, scaled_partial_pivoting]
+    print_array = ["Gaussian elimination with partial pivoting :", "Gaussian elimination with scaled partial pivoting"]
+    # gaussian elimination with different pivoting functions
+    for k in range(2):
+        print(print_array[k])
+        initialize()
+        read_augment_inputs()
+        for i in range(3):
+            answers.append(gaussian_elimination(matrices[i], pivot_functions[k]))
+            b_new.append(matrix_multiplication(a_coef[i], answers[i]))
+            error_norm = 0.0
+            for j in range(len(load_vector[i])):
+                error_norm += abs(load_vector[i][j] - b_new[i][j][0])
+            for j in range(len(answers[i])):
+                print("x"+str(j), " : ", answers[i][j])
+            print("l1-norm of error vector : ", error_norm,"\n")
+        print(100 * "/")
+        # gauss-jordan method
+        print("Gauss-jordan method")
+        initialize()
+        read_augment_inputs()
+        for i in range(3):
+            answers.append(gauss_jordan(matrices[i], partial_pivoting))
+            b_new.append(matrix_multiplication(a_coef[i], answers[i]))
+            error_norm = 0.0
+            for j in range(len(load_vector[i])):
+                error_norm += abs(load_vector[i][j] - b_new[i][j][0])
+            for j in range(len(answers[i])):
+                print("x" + str(j), " : ", answers[i][j])
+            print("l1-norm of error vector : ", error_norm, "\n")
+        print(100 * "/")
+
+
